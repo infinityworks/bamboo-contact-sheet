253c01ec92f6382fba02eb4b0ee51faafb3760e3
Dave Nicoll <david.nicoll@infinityworks.com>
2019-05-16 14:33:20 +0100

    tidy up

--- bamboo-contact-sheet.py
+++ bamboo-contact-sheet.py
@@ -2,23 +2,22 @@ from PyBambooHR import PyBambooHR
 import sys
 
 if len(sys.argv) < 2:
-    print("ERROR: Bamboo HR API key not specified.")
+    print(
+        "Syntax: bamboo-contact-sheet.py BAMBOO-API-KEY [leeds|manchester|location]")
     quit()
 
 bamboo = PyBambooHR.PyBambooHR(subdomain='infinityworks', api_key=sys.argv[1])
+
+location = ""
+if len(sys.argv) > 2:
+    location = sys.argv[2]
 
 print("Getting employee directory...")
 
 employees = bamboo.get_employee_directory()
 
-#Sort the list alphabetically on lastName
+# Sort the list alphabetically on lastName
 employees = sorted(employees, key=lambda d: d["lastName"])
-
-#for employee in employees:
-#    if employee["location"] == 'Manchester':
-#        print("{} {}, {}, {}, {}".format(employee["firstName"], employee["lastName"], employee["jobTitle"], employee["department"], employee["photoUrl"]))
-
-#print()
 
 print("Creating contact sheet...")
 
@@ -27,9 +26,20 @@ with open('output.html', 'w') as outfile:
         for line in infile:
             if "<!--content-->" in line:
                 for employee in employees:
-                    if employee["location"] == 'Manchester':
-                        outfile.write('<div class="employee"><div class="photo"><img src="{}"/></div><div class="info"><div class="name">{} {}</div><div class="title">{}</div><div class="team">{}</div></div></div>'.format(employee["photoUrl"], employee["firstName"], employee["lastName"], employee["jobTitle"], employee["department"]))
+                    if(location != ""):
+                        if employee["location"].lower() == location.lower():
+                            # print(employee)
+                            outfile.write('<div class="employee"><div class="photo"><img src="{}"/></div><div class="info"><div class="name">{} {}</div><div class="title">{}</div><div class="team">{}</div></div></div>'.format(
+                                employee["photoUrl"], employee["firstName"], employee["lastName"], employee["jobTitle"], employee["department"]))
+                    else:
+                        outfile.write('<div class="employee"><div class="photo"><img src="{}"/></div><div class="info"><div class="name">{} {}</div><div class="title">{}</div><div class="team">{}</div></div></div>'.format(
+                            employee["photoUrl"], employee["firstName"], employee["lastName"], employee["jobTitle"], employee["department"]))
             else:
                 outfile.write(line)
 
-print ("Done")
+print("Done")
+
+
+""" for employee in employees:
+    if employee["location"] == 'Manchester':
+        print('{} {}|{}|{}'.format(employee["firstName"], employee["lastName"], employee["workEmail"], employee["jobTitle"])) """
